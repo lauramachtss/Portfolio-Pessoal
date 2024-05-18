ae1fd3df34fe0a90e0f3b9d985572e1099fb3938
Laura <166519168+lauramachts0@users.noreply.github.com>
2024-05-18 15:10:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 $(document).ready(function () {
    
-    $(".fab").hover(function () {
-        $(this).css("transform", "scale(1.2)");
-    }, function () {
-        $(this).css("transform", "scale(1)");
-    });
+          $(".fab").hover(function () {
+                $(this).css("transform", "scale(1.2)");
+            }, function () {
+                $(this).css("transform", "scale(1)");
+            });
 
-  
-    $(".fab").click(function () {
-        $(this).animate({ rotation: 360 }, {
-            duration: 1000,
-            step: function (now) {
-                $(this).css({ transform: 'rotate(' + now + 'deg)' });
-            }
+            $(".fab").click(function () {
+                
+                $(this).animate({ rotation: 360 }, {
+                    duration: 1000,
+                    step: function (now) {
+                        
+                        $(this).css({ transform: 'rotate(' + now + 'deg)' });
+                    }
+                });
+            });
+            $(".fab").css("font-size", "260%");
         });
-    });
-    $(".fab").css("font-size", "250%");
-});
+        $(document).ready(function () {
+
+            $(".nav-link").hover(function () {
+                $(this).addClass("active");
+            }, function () {
+                $(this).removeClass("active");
+            });
+
+            $(".nav-link").click(function () {
+                $(this).addClass("active");
+            });
+        });
+        $(document).ready(function () {
+            
+            $(".nav-link").hover(function () {
+                $(this).css("transform", "scale(1.1)");
+            }, function () {
+                $(this).css("transform", "scale(1)");
+            });
+        });
